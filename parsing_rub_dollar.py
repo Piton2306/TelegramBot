9a27d569c89fd2f71811c77bd404cc512ef96a0a
Nastenko Peter <source-2306@mail.ru>
2024-06-13 17:30:53 +0300

    Переход на postgres

--- parsing_rub_dollar.py
+++ parsing_rub_dollar.py
@@ -1,14 +1,15 @@
 import os
 import time
 from datetime import datetime
-import logs
+
 import requests
 from bs4 import BeautifulSoup
-import sql
+
+import sql_postgress
 
 title = "Parsing dollar..."
 os.system(f"title {title}")
-
+sql_postgress.add_database()
 url = 'https://ru.investing.com/currencies/usd-rub'
 
 green = [173, 255, 47]
@@ -31,32 +32,26 @@ def get_result(url):
     return requests.get(url).text
 
 
-old_price = sql.select_last_float()
-
+old_price = sql_postgress.select_last_float()
 while True:
-    try:
-        soup = BeautifulSoup(get_result(url), "lxml")
-        price = soup.find(class_="text-5xl/9")
-        price_float = float(format(float(price.text.replace(",", ".")), '.2f'))
-        if old_price < price_float:
-            text_result_green = rgb(rgb_color=green, text=str(price_float))
-            save_file(text=str(price_float))
-            sql.insert_into_dollars(price_float, difference="↑")
-            print(text_result_green)
-            old_price = price_float
-        elif old_price > price_float:
-            text_result_red = rgb(rgb_color=red, text=str(price_float))
-            save_file(text=str(price_float))
-            sql.insert_into_dollars(price_float, difference="↓")
-            print(text_result_red)
-            old_price = price_float
-        elif old_price == price_float:
-            print(rgb(green,f"{str(datetime.now().time())[:8]} Равно {old_price} {price_float}"))
-        else:
-            print("Тест ошибки", old_price, price_float)
-        time.sleep(60)
-    except:
-        print(rgb(rgb_color=red, text=str('Ошибка')))
-        logs.loging('my_log_parsing.log')
-        time.sleep(60)
-        break
+    old_price = sql_postgress.select_last_float()
+    soup = BeautifulSoup(get_result(url), "lxml")
+    price = soup.find(class_="text-5xl/9")
+    price_float = float(format(float(price.text.replace(",", ".")), '.2f'))
+    if old_price < price_float:
+        text_result_green = rgb(rgb_color=green, text=str(price_float))
+        save_file(text=str(price_float))
+        sql_postgress.insert_into_dollars(price_float, difference="↑")
+        print(text_result_green)
+        old_price = price_float
+    elif old_price > price_float:
+        text_result_red = rgb(rgb_color=red, text=str(price_float))
+        save_file(text=str(price_float))
+        sql_postgress.insert_into_dollars(price_float, difference="↓")
+        print(text_result_red)
+        old_price = price_float
+    elif old_price == price_float:
+        print(rgb(green, f"{str(datetime.now().time())[:8]} Равно {old_price} {price_float}"))
+    else:
+        print("Тест ошибки", old_price, price_float)
+    time.sleep(60)
---
+++ sql_postgress.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+import psycopg2
+
+try:
+    # пытаемся подключиться к базе данных
+    connection = psycopg2.connect(dbname='postgres', host='localhost', port='5432', user='root', password='12345')
+    print("Подключение установлено")
+except:
+    # в случае сбоя подключения будет выведено сообщение в STDOUT
+    print('Ошибка соединения с БД')
+
+
+def add_database():
+    cursor = connection.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Dollars (
+    id SERIAL PRIMARY KEY,
+    Дата TEXT NOT NULL,
+    Время TEXT NOT NULL,
+    Цена_доллара FLOAT,
+    Комментарий TEXT,
+    Разница TEXT
+    )
+    ''')
+    connection.commit()
+
+
+def add_column(name_table, name_colum, data_type):
+    cursor = connection.cursor()
+    cursor.execute(f'''
+        ALTER TABLE {name_table} 
+        ADD {name_colum} {data_type};
+    ''')
+    connection.commit()
+
+
+# add_column('Dollars', 'Разница', "TEXT")
+
+
+def insert_into_dollars(price, difference):
+    cursor = connection.cursor()
+    date = datetime.now().date()
+    time_now = str(datetime.now().time())[:8]
+    cursor.execute(f'''INSERT INTO Dollars (Дата, Время, Цена_доллара, Разница )
+    VALUES ('{date}', '{time_now}' , {price}, '{difference}');
+    ''')
+    connection.commit()
+
+
+def select_last_telegram():
+    cursor = connection.cursor()
+    date = datetime.now().date()
+    time_now = str(datetime.now().time())[:8]
+    cursor.execute(f'''select Дата,Время,Цена_доллара from Dollars order by id desc
+limit 1;''')
+    a = cursor.fetchone()
+    cursor.close()
+    return f'{a[2]} - - {a[1]} || {a[0]}'
+
+
+def select_last_float():
+    cursor = connection.cursor()
+    cursor.execute(f'''select Дата,Время,Цена_доллара from Dollars order by id desc
+limit 1;''')
+    a = cursor.fetchone()
+    cursor.close()
+    return a[2]
+# f'''INSERT INTO Dollars (Дата, Время, Цена)
+#    VALUES ({date}, {time_now}, {price});
+#    '''
